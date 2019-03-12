56808484978197069dce723f60e89c81a94b7788
Ruben Sanchez <rubensanchezcolin@gmail.com>
2019-03-12 15:28:42 -0500

    remove notifications

--- functions.php
+++ functions.php
@@ -17,11 +17,14 @@ if ( is_admin() ) {
 }
 
 function remove_core_updates(){
-global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+    global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
 }
 add_filter('pre_site_transient_update_core','remove_core_updates');
 add_filter('pre_site_transient_update_plugins','remove_core_updates');
 add_filter('pre_site_transient_update_themes','remove_core_updates');
+
+remove_action('load-update-core.php','wp_update_plugins');
+add_filter('pre_site_transient_update_plugins','__return_null');
 
 $template_directory = get_template_directory() . '/';
 

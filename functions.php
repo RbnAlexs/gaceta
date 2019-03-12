36f97562eadae588dc57f9f7a6bb09e88e34aebc
Ruben Sanchez <rubensanchezcolin@gmail.com>
2019-03-12 15:27:43 -0500

    remove notifications

--- functions.php
+++ functions.php
@@ -16,6 +16,13 @@ if ( is_admin() ) {
     
 }
 
+function remove_core_updates(){
+global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
+
 $template_directory = get_template_directory() . '/';
 
 // Loads the loader of oculus if not included before

6d033d5ad32dd54e20939e09a2cc866f8d197281
Ruben Sanchez <rubensanchezcolin@gmail.com>
2019-03-17 14:56:30 -0600

    Sidebars fixed

--- functions.php
+++ functions.php
@@ -3,52 +3,83 @@
 
 /**
  * Registro de Sidebars
- */
-function textdomain_register_sidebars() {
+**/
 
-    /* Register the primary sidebar. */
-    register_sidebar(
-        array(
-            'id' => '001',
-            'name' => __( 'Inicio', 'textdomain' ),
-            'description' => __( 'A short description of the sidebar.', 'textdomain' ),
-            'before_widget' => '<div class="wpb_widgetised_column wpb_content_element">',
-            'after_widget' => '</div>',
-            'before_title' => '<div class="section-heading sh-t5 sh-s1"><span class="h-text">',
-            'after_title' => ' </span></div>'
-        )
-    );
+add_action( 'register_sidebar', function( $sidebar )
+{
+    global $wp_registered_sidebars;
 
-    /* Register the primary sidebar. */
-    register_sidebar(
-        array(
-            'id' => '002',
-            'name' => __( 'Historico Gacetas', 'textdomain' ),
-            'description' => __( 'A short description of the sidebar.', 'textdomain' ),
-            'before_widget' => '<div class="wpb_widgetised_column wpb_content_element">',
-            'after_widget' => '</div>',
-            'before_title' => '<div class="section-heading sh-t5 sh-s1"><span class="h-text">',
-            'after_title' => ' </span></div>'
-        )
-    );
+    if ( 'Primary Sidebar' !== $sidebar[ 'name' ] )
+        return;
 
-    /* Register the primary sidebar. */
-    register_sidebar(
-        array(
-            'id' => '003',
-            'name' => __( 'Historico Suplementos', 'textdomain' ),
-            'description' => __( 'A short description of the sidebar.', 'textdomain' ),
-            'before_widget' => '<div id="%1$s class="widget %2$s wpb_widgetised_column wpb_content_element">',
-            'after_widget' => '</div>',
-        )
-    );
+    $id = $sidebar[ 'id' ];
+    $sidebar[ 'name' ] = 'Noticias - Categoría';
 
-    /* Repeat register_sidebar() code for additional sidebars. */
-}
-add_action( 'widgets_init', 'textdomain_register_sidebars' );
+    $wp_registered_sidebars[ $id ] = $sidebar;
+});
+
+add_action( 'register_sidebar', function( $sidebar )
+{
+    global $wp_registered_sidebars;
+
+    if ( 'Secondary Sidebar' !== $sidebar[ 'name' ] )
+        return;
+
+    $id = $sidebar[ 'id' ];
+    $sidebar[ 'name' ] = 'Histórico Noticias';
+
+    $wp_registered_sidebars[ $id ] = $sidebar;
+}); 
+
+add_action( 'register_sidebar', function( $sidebar )
+{
+    global $wp_registered_sidebars;
+
+    if ( 'Footer - Column 1' !== $sidebar[ 'name' ] )
+        return;
+
+    $id = $sidebar[ 'id' ];
+    $sidebar[ 'name' ] = 'Histórico Gaceta';
+
+    $wp_registered_sidebars[ $id ] = $sidebar;
+}); 
+
+add_action( 'register_sidebar', function( $sidebar )
+{
+    global $wp_registered_sidebars;
+
+    if ( 'Footer - Column 2' !== $sidebar[ 'name' ] )
+        return;
+
+    $id = $sidebar[ 'id' ];
+    $sidebar[ 'name' ] = 'Histórico Suplemento';
+    $wp_registered_sidebars[ $id ] = $sidebar;
+}); 
+
+add_action( 'register_sidebar', function( $sidebar )
+{
+    global $wp_registered_sidebars;
+
+    if ( 'Footer - Column 3' !== $sidebar[ 'name' ] )
+        return;
+
+    $id = $sidebar[ 'id' ];
+    $sidebar[ 'name' ] = 'Histórico Video';
+    $wp_registered_sidebars[ $id ] = $sidebar;
+}); 
 
 
- 
+add_action( 'register_sidebar', function( $sidebar )
+{
+    global $wp_registered_sidebars;
+
+    if ( 'Footer - Column 4' !== $sidebar[ 'name' ] )
+        return;
+
+    $id = $sidebar[ 'id' ];
+    $sidebar[ 'name' ] = 'Página de Inicio';
+    $wp_registered_sidebars[ $id ] = $sidebar;
+}); 
 
 
 
